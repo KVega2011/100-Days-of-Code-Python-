5326b9e522cff19bd71f24b37b46dff1f1bc67f2
Kailyn Vega <kailynvega@kailyns-air.lan>
2023-12-16 07:42:42 -0600

    Day 1 complete!

---
+++ dayOne.py
@@ -0,0 +1,20 @@
+print("Hello there! I am here to ask you a few questions.")
+print()
+name = input("What is your name? ")
+print()
+food = input("What is your favorite food? ")
+print()
+music = input("What is your favorite music? ")
+print()
+live = input("Where do you live? ")
+
+print("You are "
++name
++" You're probably hungry for "
++food
++" and you're definitely getting your groove on to "
++music
++" living in the amazing "
++live
++" Where you will have a nice partner ")
+
