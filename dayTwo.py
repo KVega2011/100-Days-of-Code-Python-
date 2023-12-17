7129a61570a55fa7f682abd9794885700cfa82e4
Kailyn Vega <kailynvega@kailyns-air.lan>
2023-12-17 11:24:26 -0600

    Day 2 complteted!

---
+++ dayTwo.py
@@ -0,0 +1,7 @@
+food = input("name a food " )
+plant = input("Name a type of plant ")
+cooking = input("Name a method of cooking ")    
+burned = input("What word describes burned food? ")
+item = input("Name a DIY item ")
+print("MENU")
+print(cooking, food ,"with" , burned, plant, "on a bed of", item)
