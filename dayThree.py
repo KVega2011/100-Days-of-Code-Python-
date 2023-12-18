521dbf93616247d31e922b2ed4c386bb2c24cb98
Kailyn Vega <kailynvega@kailyns-air.lan>
2023-12-18 11:11:25 -0600

    Day 4 completed!

---
+++ dayThree.py
@@ -0,0 +1,10 @@
+print("Welcome to the choose your own story!")
+print("You are a detective and you have been assigned a case.")
+print("you are in a haunted house and you have to find the murderer.")
+murderer = input("What is gonna be the murderers name? ")
+victim = input("What is gonna be the victims name? ")
+survivor = input("What is gonna be the survivors name? ")
+food = input("What is gonna be the food? ")
+
+print("Yesterday, ",victim , " was murdered in the mc mansion house. The murderer was", murderer,"But you dont know that yet, So you go to the house to find out more about the murder.The house owner gave you a cup of coffee! How nice of her.Then You go to the kitchen and you see evidence that the leftover", food," left on the kitchen table was eaten by someone.So you go to ask", survivor," who ate the food", survivor, " says that", victim,
+"was eating the food and that", murderer,"had prepared it. You go to the living room and you see the", murderer,"smiling at you weirdly.But you are done! you solved the case and you are a hero right? Well it turns out that the house owner was the murderer, So you drank poison and as you fade unconscious you see the murderer smiling at you. Preparing for you to be their next victim. The End.")
