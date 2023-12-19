fb74261533127494cc5d8927fc3a93152054d532
Kailyn Vega <kailynvega@kailyns-air.lan>
2023-12-19 10:02:17 -0600

    Day four completed!

---
+++ dayFour.py
@@ -0,0 +1,24 @@
+print("Welcome to the Everybody hates chris generator!")
+rochelle = input("Do you like to yell alot? ")
+if rochelle == "yes":
+  print("You are a part of rochelle!")
+else: 
+  print("You are not a part of rochelle!")
+  
+drew = input("Do girls/boys flock to you? ")
+if drew == "yes":
+  print("You are a part of drew!")
+else: 
+  print("You are not a part of drew!")
+  
+chris = input("Are you an disliked older sibling? ")
+if chris == "yes":
+  print("I am so sorry but atleast you are a part of chris!")
+else: 
+  print("You are not a part of chris!")
+
+julius = input("Do you save money alot and complain about cents being wasted? ")
+if julius == "yes":
+  print("You are a part of julius!")
+else:
+  print("You are not a part of julius!")
