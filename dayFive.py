3c79a7f72c6747aefbee479a201aa700c03518aa
Kailyn Vega <kailynvega@kailyns-air.lan>
2023-12-20 16:42:07 -0600

    Day five completed!

---
+++ dayFive.py
@@ -0,0 +1,11 @@
+print("Welcome this is where you Log in!")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == "tammy" and password == "Milkycow":
+  print("Welcome Tammy! I hope your having a wonderful day! I know your new bf philp is bothering you and i hope everything goes well and he gets taken care of!")
+elif username == "amy" and password == "Milkcow":
+  print("Welcome Amy! Congrats on becoming a new mom. I know it isnt easy but its going to be worth it! I hope you have a wonderful day!")
+elif username == "chris" and password == "Milkycows":
+  print("Welcome Chris! Congrats on getting that weight loss surgery. I hope you recover well and I know you are going to have a great day!")
+else:
+ print("Sorry, I don't know you. Please try again!")
