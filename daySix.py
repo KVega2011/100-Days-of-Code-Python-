774bff26c43aa0a89ed89dbbfb01ff7275e09f5f
Kailyn Vega <kailynvega@kailyns-air.lan>
2023-12-21 11:28:19 -0600

    Day six completed!

---
+++ daySix.py
@@ -0,0 +1,16 @@
+print("Are you a TRUE fan of eveybody hates chris? Lets find out!")
+chris = input("Who is chris best friend? ")
+if chris == "greg":
+ print("You are correct!")
+else:
+ print("Cmon this was an EASY one!")  
+killmove = input("Who is the name of the homeless guy on the street? ")
+if killmove == "killmove":
+  print("You are correct! I know you atleast watched the show now!")
+else:
+ print("Keep trying this one was a bit harder for others as well!")
+tasha = input("Who is the girl Chris dated? ")
+if tasha == "tasha":
+  print("You are correct! I knew you were a true fan!")
+else: 
+  print("You are wrong! It looks like your not a true fan afterall.")
